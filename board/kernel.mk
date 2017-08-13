1f8e7b0f80d5ea95ca9c06b08020e69ad287d69b
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2017-08-13 18:58:12 +0600

    ace: Rename kernel config
    Also rename folder because i don't support HTC Desire S

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,6 +1,6 @@
 # Kernel
-TARGET_KERNEL_CONFIG := ace_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/msm7x30
+TARGET_KERNEL_CONFIG := lineageos_ace_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/ace
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 androidboot.selinux=permissive androidboot.hardware=htc7x30
 BOARD_KERNEL_RECOVERY_CMDLINE := $(BOARD_KERNEL_CMDLINE) msmsdcc_power_gpio=88
 BOARD_KERNEL_BASE := 0x4000000
