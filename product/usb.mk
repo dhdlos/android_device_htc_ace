5678037d9224b352a7bcd69888086fd7fc747f4d
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2017-08-15 23:36:44 +0600

    ace: Enable MIDI support
    Add the required USB compositions and declare feature support.

--- product/usb.mk
+++ product/usb.mk
@@ -9,4 +9,5 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
