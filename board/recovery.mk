85b5fdfc814529c92967d2100d901319f224c9a1
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2017-08-26 14:59:42 +0300

    ace: Renamed [2/2]

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,5 +1,5 @@
 # Recovery
 TARGET_RECOVERY_DEVICE_DIRS += device/htc/ace
-TARGET_RECOVERY_FSTAB := device/htc/ace/rootdir/fstab.htc7x30
+TARGET_RECOVERY_FSTAB := device/htc/ace/rootdir/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 BRIGHTNESS_SYS_FILE := /sys/devices/platform/leds-pm8058/leds/keyboard-backlight/brightness
