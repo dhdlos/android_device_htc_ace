80c87825e6c3da28210003e69d603e67da5b0579
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2017-08-26 14:55:10 +0300

    ace: For now is without lineageos prefix

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,5 +1,5 @@
 # Kernel
-TARGET_KERNEL_CONFIG := lineageos_ace_defconfig
+TARGET_KERNEL_CONFIG := ace_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/ace
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 androidboot.selinux=permissive androidboot.hardware=htc7x30
 BOARD_KERNEL_RECOVERY_CMDLINE := $(BOARD_KERNEL_CMDLINE) msmsdcc_power_gpio=88
